5dbe5b7b7b5c4a0be9f9910c2ad721897f8e1456
echekholskiy <62834709+echekholskiy@users.noreply.github.com>
2020-04-29 10:55:59 +0300

    rename variable of Loc in ProjectPage

--- src/main/java/pages/ProjectPage.java
+++ src/main/java/pages/ProjectPage.java
@@ -15,12 +15,12 @@ public class ProjectPage {
         this.driver = driver;
     }
 
-    private By locAddButtonLoc=By.xpath("//div[@class='project new']");
-    private By locProjectButtonsLoc=By.xpath("//div[@class='project']/a/div[1]");
+    private By addButtonLoc=By.xpath("//div[@class='project new']");
+    private By projectButtonsLoc=By.xpath("//div[@class='project']/a/div[1]");
 
 
     public CreateProjectPage clickAddProject(){
-        WebElement addProjectButton=driver.findElement(locAddButtonLoc);
+        WebElement addProjectButton=driver.findElement(addButtonLoc);
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView(true);", addProjectButton);
         addProjectButton.click();
@@ -29,7 +29,7 @@ public class ProjectPage {
 
     //TODO переписать метод без List
     public WebElement getLastOfProject(){
-        List<WebElement> listOfProjects=driver.findElements(locProjectButtonsLoc);
+        List<WebElement> listOfProjects=driver.findElements(projectButtonsLoc);
         return listOfProjects.get(listOfProjects.size()-1);
     }
 
