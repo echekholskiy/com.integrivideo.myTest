2af34a1d08fc178adff414ba541a2e1d11410f96
echekholskiy <62834709+echekholskiy@users.noreply.github.com>
2020-04-29 10:43:59 +0300

    rename variable of Loc in InProjectPage

--- src/main/java/pages/InProjectPage.java
+++ src/main/java/pages/InProjectPage.java
@@ -10,26 +10,25 @@ public class InProjectPage {
         this.driver = driver;
     }
 
-    //TODO это че xpath?
-    private By h1=By.xpath("//h1"); //TODO h1-переименовать
-    private By locDescription=By.xpath("//div[@class='col-12 description']");
-    private By editButton=By.xpath("//a[text()='Edit']");
-    private By addComponentButton=By.xpath("//div[@class='status']");
+    private By projectNameLoc=By.xpath("//h1");
+    private By descriptionLoc=By.xpath("//div[@class='col-12 description']");
+    private By editButtonLoc=By.xpath("//a[text()='Edit']");
+    private By addComponentButtonLoc=By.xpath("//div[@class='status']");
 
     public String getProjectName(){
-        return driver.findElement(h1).getText();
+        return driver.findElement(projectNameLoc).getText();
     }
     public String getDescription(){
-        return driver.findElement(locDescription).getText();
+        return driver.findElement(descriptionLoc).getText();
     }
 
     public EditProjectPage clickEditButton(){
-        driver.findElement(editButton).click();
+        driver.findElement(editButtonLoc).click();
         return new EditProjectPage(driver);
     }
 
     public NewComponentPage clickAddComponentButton(){
-        driver.findElement(addComponentButton).click();
+        driver.findElement(addComponentButtonLoc).click();
         return new NewComponentPage(driver);
     }
 
